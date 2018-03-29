09845a2a6e0c43c459f897d130770204a8627482
Minjae Kim <jwya2149@dgist.ac.kr>
2018-03-29 14:25:40 +0900

    Make virtual array and bad-block considerations with debug
    1. Debug - fix linking error with inline functions, change numValid datatype: uint8_t -> int16_t
    2. Virtual PBA - make virtual PBA functions
    3. Bad block consideration - special initailzation and heap structure of bad blocks

--- blockmanager/BM_Heap.c
+++ blockmanager/BM_Heap.c
@@ -12,7 +12,10 @@ int32_t BM_Minheap_numValid(Block* blockArray, nV_T** numValid_map)
 	nV_T* temp_NV = (nV_T*)malloc(sizeof(nV_T) * _NOB);
 
 	for (int i = 0; i < _NOB; ++i) {
-		temp_NV[i] = blockArray[i].numValid;
+		if (blockArray[i].BAD == _NOTBADSTATE)
+			temp_NV[i] = blockArray[i].numValid;
+		else
+			temp_NV[i] = 0xff; // Bad Block would go to leaf node.
 	}
 
 	BM__buildminheapNV(temp_NV, numValid_map); 
@@ -36,10 +39,10 @@ int32_t BM__minheapifyNV(nV_T* temp_NV, int32_t i, nV_T** numValid_map)
 	int32_t r = 2 * i + 2;
 	int32_t smallest;
 
-	if (l <= _NOB - 1 && temp_NV[l] < temp_NV[i]) smallest = l;
+	if (l <= _NOB - 1 && (unsigned)temp_NV[l] < (unsigned)temp_NV[i]) smallest = l;
 	else smallest = i;
 
-	if (r <= _NOB - 1 && temp_NV[r] < temp_NV[smallest])	smallest = r;
+	if (r <= _NOB - 1 && (unsigned)temp_NV[r] < (unsigned)temp_NV[smallest])	smallest = r;
 	if (smallest != i) {                      
 		
 		SWAP_NV(temp_NV[i], temp_NV[smallest]);
@@ -55,6 +58,7 @@ int32_t BM__minheapifyNV(nV_T* temp_NV, int32_t i, nV_T** numValid_map)
  */
 
  /* Make Max-Heap with the pointers of numValid_map by numValid in blockArray */
+// This function is not used..
 int32_t BM_Maxheap_numValid(Block* blockArray, nV_T** numValid_map)
 {
 	nV_T* temp_NV = (nV_T*)malloc(sizeof(nV_T) * _NOB);
@@ -110,6 +114,7 @@ int32_t BM_Minheap_PEcycle(Block* blockArray, PE_T** PE_map)
 	for (int i = 0; i < _NOB; ++i) {
 		temp_PE[i] = blockArray[i].PE_cycle;
 	}
+
 
 	BM__buildminheapPE(temp_PE, PE_map); 
 
@@ -189,3 +194,5 @@ void BM__quicksort(PE_T* temp_PE, int p, int r, PE_T** PE_map)
 		BM__quicksort(temp_PE, q + 1, r, PE_map);
 	}
 }
+
+/* If we want Maxheap_PE, comparing by (signed) casting. Then, PE of bad block 0xffffffff would be -1, so bad block will go to leaf nodes. */
--- blockmanager/BM_Main.c
+++ blockmanager/BM_Main.c
@@ -4,13 +4,14 @@
 
 int main(void)
 {
-    printf("********************* Bad-Block Manager *********************\n");
+    printf("********************* Block Manager *********************\n");
     printf("Start main!\n\n");
     BM_Init();
 
 
 
 	printf("BM_Init End in MAIN!!!\n");
+
 	
 	BM_Shutdown();
 	printf("BM_Shutdown End in MAIN!!\n");
