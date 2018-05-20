f15ddcb66ecd8d38e914467833021fbee3e97ce5
wlsrnjsgb@gmail.com <wlsrnjsgb@gmail.com>
2018-05-21 03:12:50 +0900

    Documentation for fast_AllocSWLogBlockEntry

--- algorithm/Hybrid/fast_AllocSWLogBlockEntry.c
+++ algorithm/Hybrid/fast_AllocSWLogBlockEntry.c
@@ -1,26 +1,21 @@
 #include "FAST.h"
 
 /**
- * Function :
- * 
- * Description :
- *  Branch 0: Is SW Log Block is full
- *   true : Switch SW Log Block
- *
- *  Branch 1: Is offset is 0
- *   true : Merge SW Log Block and make allocation with given key
- *   false : go to Branch 2
- *  Branch 2: Is block is same
- *   true : go to Branch 3 
- *   false : return eNOTSEQUENTIAL 
- *  Branch 3: Is offset if sequential
- *   true : Allocate 
- *   false : return eNOTSEQUENTIAL
- *
- * Returns :
- *  No returns
+ * @brief		Allocate empty block in SW Log Block
+ * @details		Branch 1: If offset if 0 \n
+ * 				 true : Merge SW Log Block and make allocation with given key \n
+ * 				 false: Go to Branch 2 \n
+ * 				Branch 2: If block number is same \n
+ * 				 true : Go to Branch 3 \n
+ * 				 false: Return eNOTSEQUENTIAL \n
+ * 				Branch 3: If offset is sequential \n
+ * 				 true : Allocate SW log block entry \n
+ * 				 false: Return eNOTSEQUENTIAL \n
+ * @param		key					TODO
+ * @param		physical_address	TODO
+ * @param		req					TODO
+ * @return		Error code
  */
-
 char fast_AllocSWLogBlockEntry(KEYT key, uint32_t* physical_address, request *const req)
 {
     //int logical_block;
@@ -36,23 +31,23 @@ char fast_AllocSWLogBlockEntry(KEYT key, uint32_t* physical_address, request *co
     logical_block = sw_MappingInfo->logical_block;
     sw_log_block = sw_MappingInfo->sw_log_block;
    	
-	if(sw_MappingInfo->number_of_stored_sector == PAGE_PER_BLOCK){
+	if (sw_MappingInfo->number_of_stored_sector == PAGE_PER_BLOCK) {
 		fast_SwitchSWLogBlock();
         sw_log_block = sw_MappingInfo->sw_log_block;
         logical_block = sw_MappingInfo->logical_block;
 	}
 	
-	if(offset == 0 && GET_PAGE_STATE(ADDRESS(sw_log_block, 0)) != ERASED){
+	if (offset == 0 && GET_PAGE_STATE(ADDRESS(sw_log_block, 0)) != ERASED) {
         fast_MergeSWLogBlock(req);
         sw_log_block = sw_MappingInfo->sw_log_block;
 	}
-	else if(block == logical_block){
-		if(offset != sw_MappingInfo->number_of_stored_sector){
+	else if (block == logical_block) {
+		if (offset != sw_MappingInfo->number_of_stored_sector) {
 		    fast_MergeSWLogBlock(req);
 			return (eNOTSEQUENTIAL);
 		}
 	}
-	else{
+	else {
 		return (eNOTSEQUENTIAL);
 	}
     
