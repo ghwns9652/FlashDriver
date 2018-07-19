8a73e6d50936ed6ff3173ea67210a1268f32954c
DGitH <ghwns9652@dgist.ac.kr>
2018-07-19 17:42:33 +0900

    add logic on normal folder.

--- algorithm/normal/__normal.c
+++ algorithm/normal/__normal.c
@@ -14,28 +14,31 @@ struct algorithm __normal={
 	.remove=normal_remove
 };
 
-typedef struct ppa_table{
+typedef struct ppa_node{
 	char valid;
 	uint32_t lpa;
 } ppa_node;
 
-typedef struct lpa_table{
+typedef struct lpa_node{
 	uint32_t ppa;
 	char valid;
+	char *value;
 } lpa_node;
 
 
 static lpa_node *l_table;
 static ppa_node *p_table;
 
-uint32_t keyset(KEYT k){
+void setkey(request *const req ,KEYT k){
 	//non-occupied
 	if (l_table[k].valid == 0) {
 		l_table[k].valid = 1;
 		l_table[k].ppa = rand() %200;
 		p_table[l_table[k].ppa].lpa = k;
 		p_table[l_table[k].ppa].valid = 1;
-		return (uint32_t)l_table[k].ppa;
+
+		req->key = l_table[k].ppa;
+		return;
 	}
 	//pre-occupied
 	else if (l_table[k].valid == 1) {
@@ -50,16 +53,25 @@ uint32_t keyset(KEYT k){
 		p_table[k].valid = 0;
 		p_table[l_table[k].ppa].valid = 1;
 		p_table[l_table[k].ppa].lpa = k;
-		return (uint32_t)l_table[k].ppa;
+
+		req->key = l_table[k].ppa;
+		return;
 	}
-	return 0;
+	return;
 }
 
-uint32_t keypull(KEYT k){
+void getkey(request * const req,KEYT k){
 	if(l_table[k].valid == 1){
-		return l_table[k].ppa;
+		req->key = l_table[k].ppa;		
+		return;
 	}
-	return 0;
+	return;
+}
+
+void setvalue(request * const req, KEYT k){
+	char* tmp_value = (char*)malloc(sizeof(char)*PAGESIZE);
+	memset(tmp_value,(char)k,PAGESIZE);
+	req->value->value = tmp_value;
 }
 
 uint32_t normal_create (lower_info* li,algorithm *algo){
@@ -71,15 +83,19 @@ uint32_t normal_create (lower_info* li,algorithm *algo){
 	return 1;
 }
 void normal_destroy (lower_info* li, algorithm *algo){
-	printf("\nsort by PPA\n");
-	for(int i=0;i<200;i++)
-		printf("ppa: %u valid:%d lpa:%u\n",i,(int)p_table[i].valid,p_table[i].lpa);
-	printf("\nsort by LPA\n");
-	for(int i=0;i<100;i++)
-		printf("lpa: %u ppa:%u\n",i,l_table[i].ppa);
-
+	printf("\n-------sort by PPA-------\n");
+	for(int i=0;i<200;i++){
+		printf(" ppa: %u valid:%c lpa:%u \n",i,p_table[i].valid,p_table[i].lpa);
+	}
+	printf("\n--------sort by LPA-------\n");
+	for(int i=0;i<100;i++){
+		char *tmp = l_table[i].value;
+		printf(" lpa: %u ppa:%u value: %d \n",i,l_table[i].ppa,(int)tmp[0]);
+//	printf("\n-------------------------\n");
+	}
 	return;
 }
+
 uint32_t normal_get(request *const req){
 	bench_algo_start(req);
 	normal_params* params=(normal_params*)malloc(sizeof(normal_params));
@@ -91,9 +107,9 @@ uint32_t normal_get(request *const req){
 	my_req->params=(void*)params;
 	bench_algo_end(req);
 
-	KEYT mapped_key = keypull(req->key); //ppa -> lpa
+	getkey(req,req->key); //ppa -> lpa
 
-	__normal.li->pull_data(mapped_key,PAGESIZE,req->value,req->isAsync,my_req);
+	__normal.li->pull_data(req->key,PAGESIZE,req->value,req->isAsync,my_req);
 	return 1;
 }
 uint32_t normal_set(request *const req){
@@ -106,10 +122,11 @@ uint32_t normal_set(request *const req){
 	my_req->end_req=normal_end_req;
 	my_req->params=(void*)params;
 	bench_algo_end(req);
-	
-	KEYT mapped_key = keyset(req->key); //lpa -> ppa
 
-	__normal.li->push_data(mapped_key,PAGESIZE,req->value,req->isAsync,my_req);
+	setvalue(req,req->key);
+	setkey(req,req->key); //lpa -> ppa
+
+	__normal.li->push_data(req->key,PAGESIZE,req->value,req->isAsync,my_req);
 	return 1;
 }
 uint32_t normal_remove(request *const req){
@@ -119,6 +136,19 @@ uint32_t normal_remove(request *const req){
 
 void *normal_end_req(algo_req* input){
 	normal_params* params=(normal_params*)input->params;
+
+	//set
+	if(params->test == 5) {
+		char *tmp_value  = input->parents->value->value;
+		uint32_t tmp_lpa = input->parents->key;
+		l_table[tmp_lpa].value = tmp_value;
+	}
+//	//get
+//	else if(params->test == 9) { 
+//		char *tmp_value  = input->parents->value->value;
+//		uint32_t tmp_lpa = p_table[input->parents->key].lpa;
+//		l_table[tmp_lpa].value = tmp_value;
+//	}
 	
 	request *res=input->parents;
 	res->end_req(res);
