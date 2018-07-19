c88f405248f341c0d12407624337bbcd019248bd
jiho0307 <jiho0307@dgist.ac.kr>
2018-07-19 20:05:53 +0900

    BM change

--- algorithm/blockmanager/BM_Block.c
+++ algorithm/blockmanager/BM_Block.c
@@ -1,65 +1,32 @@
-/* Structure Interface */
-
 #include "BM.h"
 
-int32_t numBlock;
-int32_t PagePerBlock;
+/* 
+ * (README)
+ * This File(Block.c) is incomplete.
+ * You should consider the hardware function is not provided yet!
 
-/* Initiation of Block Manager */
-int32_t BM_Init(Block** blockArray)
-{
-	numBlock = _NOS;
-	PagePerBlock = _PPS;
+ * (2018.07.01) Reduce useless features 
+ */
 
-	*blockArray = (Block*)malloc(sizeof(Block) * numBlock);
+/* Declaration of Data Structures */
 
-	/* Initialize blockArray */
-	BM_InitBlockArray(*blockArray);
-
-	printf("blockArray Test 0~4\n");
-	for (int i=0; i<5; ++i){
-		printf("PBA: %d\n", (*blockArray)[i].PBA);
-		printf("Invalid: %d\n", (*blockArray)[i].Invalid);
-	}
-	
-	printf("BM_Init() End!\n");
-	return 0;
-}
-
-/* Initalize blockArray */
-int32_t BM_InitBlockArray(Block* blockArray)
-{
-	int numItem = BM_GetnumItem();
-
-	for (int i=0; i<numBlock; ++i){
-		blockArray[i].PBA = i;
-		blockArray[i].Invalid = 0;
-		blockArray[i].hn_ptr = NULL;
-		blockArray[i].type = 0;
-
-		/* Initialization with INVALIDPAGE */
-		/* for (int j=0; j<PagePerBlock; ++j)
-			blockArray[i].ValidP[j] = BM_INVALIDPAGE;
-		memset(blockArray[i].ValidP, BM_INVALIDPAGE, sizeof(ValidP_T)*4); */
-
-		/* Initialization with VALIDPAGE */
-		blockArray[i].ValidP = (ValidP_T*)malloc(numItem);
-		for (int j=0; j<numItem; ++j)
-			blockArray[i].ValidP[j] = BM_VALIDPAGE;
+/* Initiation of Bad-Block Manager */
+int32_t BM_Init(Block **blockarray){
+	int32_t nob = _NOS;
+	*blockarray = (Block*)malloc(sizeof(Block) * nob);
+	for (int i = 0; i < nob; i++){
+		(*blockarray)[i].PBA = i;
+		(*blockarray)[i].Invalid = 0;
+		(*blockarray)[i].hn_ptr = NULL;
+		(*blockarray)[i].type = 0;
 	}
 	return 0;
 }
 
-/* Shutdown of Block structures */
-int32_t BM_Free(Block* blockArray)
-{
-	for (int i=0; i<numBlock; ++i)
-		free(blockArray[i].ValidP);
-	free(blockArray);
-	return 0;
+void BM_Free(Block *blockarray){
+	free(blockarray);
 }
 
-/* Heap Interface Functions */
 Heap* BM_Heap_Init(int max_size){
 	return heap_init(max_size);
 }
@@ -84,15 +51,16 @@ Block* BM_Heap_Get_Max(Heap *heap){
 	max_heapify(heap);
 	res = (Block*)heap->body[0].value;
 	res->hn_ptr = NULL;
-	heap->body[0].value = heap->body[heap->idx - 1].value;
-	heap->body[heap->idx - 1].value = NULL;
-	first = (Block*)heap->body[0].value;
-	first->hn_ptr = &heap->body[0];
+	if(heap->idx != 1){
+		heap->body[0].value = heap->body[heap->idx - 1].value;
+		heap->body[heap->idx - 1].value = NULL;
+		first = (Block*)heap->body[0].value;
+		first->hn_ptr = &heap->body[0];
+	}
 	heap->idx--;
 	return res;
 }
 
-/* Queue Interface Functions */
 void BM_Queue_Init(b_queue **q){
 	initqueue(q);
 }
