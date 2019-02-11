274ef3893b62c38c45761a926e4bf2b5cd22542d
Jinuc <2inuc.bae@gmail.com>
2019-02-11 18:29:22 +0900

    [Demand] Initial commit on demand branch

---
+++ algorithm/demand/demand.h
@@ -0,0 +1,44 @@
+
+/*
+ * Demand FTL Header File
+ */
+
+#ifndef __DEMAND_H__
+#define __DEMAND_H__
+
+#include "lru.h"
+#include "caching.h"
+
+#define DEMAND_EXIT_SUCCESS 0
+#define DEMAND_EXIT_FAILURE 1
+
+/* Structures */
+struct demand_handle {
+	// CMT
+	// 
+};
+
+/* Functions */
+
+// ++ demand.c
+uint32_t demand_create(lower_info*, algorithm*);
+void     demand_destroy(lower_info*, algorithm*);
+uint32_t demand_read(request *const);
+uint32_t demand_write(request *const);
+uint32_t demand_remove(request *const);
+// -- demand.c
+
+// ++ demand_internal.c
+uint32_t __demand_create(lower_info*, algorithm*);
+void     __demand_destroy(lower_info*, algorithm*);
+uint32_t __demand_read(request *const);
+uint32_t __demand_write(request *const);
+uint32_t __demand_remove(request *const);
+// -- demand_internal.c
+
+// ++ gc.c
+uint32_t data_gc();
+uint32_t trans_gc();
+// -- gc.c
+
+#endif
