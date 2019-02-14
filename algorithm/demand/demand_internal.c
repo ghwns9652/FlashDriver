0c8c1a73de8aab965f6d2ad0ed5abc5f4da48cd3
Yumin Won <wym7787@dgist.ac.kr>
2019-02-14 12:30:22 +0900

    Add caching layer

--- algorithm/demand/demand_internal.c
+++ algorithm/demand/demand_internal.c
@@ -1,8 +1,5 @@
 #include "dftl.h"
 
-static int compare_key() {
-
-}
 
 uint32_t __demand_read(request *const req) {
 
--- algorithm/demand/lru.h
+++ algorithm/demand/lru.h
@@ -1,4 +1,28 @@
 #ifndef __LRU_H__
 #define __LRU_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+struct __node{
+	void *data;           //translation page pointer (Page unit) for DFTL, SFTL
+	struct __node *next;
+	struct __node *prev;
+}NODE;
+
+
+typedef struct __lru{
+	int size;
+	NODE *head;
+	NODE *tail;
+}LRU;
+
+void lru_init(LRU**);
+void lru_free(LRU*);
+NODE* lru_push(LRU*, void*);
+void* lru_pop(LRU*);
+void lru_update(LRU*, NODE*);
+void lru_delete(LRU*, NODE*);
+
 #endif
